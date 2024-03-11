511011c3674e3d9cc1393b05ba80de2375985b17
Avi Mukesh <avimukesh10@gmail.com>
2024-03-11 00:03:15 +0000

    checking win

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,21 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import { SquareState } from "../squareState";
 import { socket } from "../app/socket";
-import { useSelector } from "react-redux";
 import useGameState from "../context/GameContext";
 
 export const Square = ({ coordinates }) => {
   const { boardState, myMove } = useGameState();
 
-  const [squareState, setSquareState] = useState(SquareState.EMP);
+  const isFilled =
+    boardState[coordinates.x][coordinates.y] !== SquareState.EMPTY;
 
   const handleClick = () => {
-    myMove(coordinates);
-    socket.emit("made_move", { coordinates });
+    if (!isFilled) {
+      myMove(coordinates);
+      socket.emit("made_move", { coordinates });
+    }
   };
 
   return (
-    <div className="board__square" onClick={handleClick}>
+    <div
+      className="board__square"
+      onClick={handleClick}
+      style={{ cursor: isFilled ? "default" : "pointer" }}
+    >
       {boardState[coordinates.x][coordinates.y]}
     </div>
   );
--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -8,6 +8,7 @@ const GameContext = createContext(null);
 
 export const GameProvider = ({ children }) => {
   const { symbol, opponentSymbol } = usePlayerContext();
+  const [lastMoveCoordinates, setLastMoveCoordinates] = useState(null);
 
   const [boardState, setBoardState] = useState([
     [SquareState.EMPTY, SquareState.EMPTY, SquareState.EMPTY],
@@ -22,6 +23,7 @@ export const GameProvider = ({ children }) => {
     const newBoardState = [...boardState];
     newBoardState[coordinates.x][coordinates.y] = SquareState[symbol];
     setBoardState(newBoardState);
+    setLastMoveCoordinates(coordinates);
   };
 
   const opponentMadeMove = ({ coordinates }) => {
@@ -31,7 +33,31 @@ export const GameProvider = ({ children }) => {
     const newBoardState = [...boardState];
     newBoardState[coordinates.x][coordinates.y] = SquareState[opponentSymbol];
     setBoardState(newBoardState);
+    setLastMoveCoordinates(coordinates);
   };
+
+  useEffect(() => {
+    if (lastMoveCoordinates) {
+      const { x, y } = lastMoveCoordinates;
+      const sameSymbol = (symbol) => {
+        console.log(symbol);
+        return symbol === boardState[y][x];
+      };
+
+      const sameRow = boardState[y].every(sameSymbol);
+
+      const sameCol = boardState.map((row) => row[x]).every(sameSymbol);
+
+      const sameDiag =
+        x === y
+          ? boardState.map((row, index) => row[index]).every(sameSymbol)
+          : false;
+
+      console.log(`same row: ${sameRow}`);
+      console.log(`same col: ${sameCol}`);
+      console.log(`same diag: ${sameDiag}`);
+    }
+  }, [lastMoveCoordinates, boardState]);
 
   useEffect(() => {
     socket.on("made_move", opponentMadeMove);
